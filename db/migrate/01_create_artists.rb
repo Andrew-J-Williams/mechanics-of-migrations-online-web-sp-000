fe4ddaa90fb163475ace730762add4919b0fa82d
Andrew Williams <1williamsaj@gmail.com>
2019-11-27 03:51:46 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -1,6 +1,11 @@
 class CreateArtists < ActiveRecord::Migration[5.2]
+  def up
+  end
 
-  def change
+  def down
+  end
+
+  def change #primary way of writing migrations
     create_table :artists do |t|
       t.string :name
       t.string :genre
@@ -8,11 +13,4 @@ class CreateArtists < ActiveRecord::Migration[5.2]
       t.string :hometown
     end
   end
-
-  def up
-  end
-
-  def down
-  end
-
 end
